a4f758b38e810fe6a3c7aec9743573518ad2cac8
Konstantin Nochevny <kosnobiz@gmail.com>
2024-05-08 22:10:57 +0300

    Style layout header and resume

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,18 @@
 <template>
 
     <header class="app-header">
-        <a href="/">
+        <a href="#top">
             <p>kNoCoding</p>
         </a>
 
         <ul>
-            <li><a href="#hero">Hero</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#resume">Resume</a></li>
             <li><a href="#contact">Contact</a></li>
         </ul>
+
+        <button>menu</button>
     </header>
 
 </template>
