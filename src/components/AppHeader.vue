12ffe99cb31fe4ac2f56331df561e3c18f076935
Konstantin Nochevny <kosnobiz@gmail.com>
2024-05-21 22:58:19 +0300

    Change box shadow properties

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -37,7 +37,7 @@
                 background-color: $color-background;
                 // TODO: fix drop shadow on these buttons
                 box-shadow:
-                    -5px -5px 10px rgba(255, 255, 255, 0.15),
+                    -4px -4px 4px rgba(255, 255, 255, 0.15),
                     5px 5px 10px rgba(0, 0, 0, 0.25);
                 // box-shadow:
                 //     -4px -4px 4px rgba(255, 255, 255, 0.25),
